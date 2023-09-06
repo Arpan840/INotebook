eb2e93062bb07701e4c79e73bc0d61bacace6c68
Arpan Das <arpan.das@klythe.com>
2023-09-06 12:21:50 +0530

    changed index

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
-const mongoose=require("mongoose");
-const db="mongodb+srv://arpandas020498:lKSREfJlmnnaP2IL@cluster0.hlq0cta.mongodb.net/INotebook?retryWrites=true&w=majority"
-mongoose.connect(db).then(()=>{
-    console.log("Connected to mongoDb");
-}).catch((err)=>{
-    console.log(err)
-})
-module.exports=mongoose;
+const mongoose = require('mongoose');
+
+const db = 'mongodb+srv://arpandas020498:lKSREfJlmnnaP2IL@cluster0.hlq0cta.mongodb.net/INotebook?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+module.exports = mongoose;
