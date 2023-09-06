cd77c9aadc89ee07afbe5b3ee0b37a00135c1918
Arpan Das <arpan.das@klythe.com>
2023-09-06 12:29:46 +0530

    create index added

--- Models/Notes.js
+++ Models/Notes.js
@@ -1,27 +1,29 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const notesSchema=new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      },
-      
-title:{
-    type:String,
-    require:true
+const notesSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+  },
+  tag: {
+    type: String,
+    default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-},
-description:{type:String,
-  
-},
-tag:{
-    type:String,
-   default:"General"
-},
-date:{
-    type:Date,
-    default:Date.now
-}
-})
-const Notes=new mongoose.model("Notes",notesSchema);
-module.exports=Notes;
+const Notes = new mongoose.model("Notes", notesSchema);
+
+Notes.collection.createIndex({ user: 1 });
+
+module.exports = Notes;
--- Models/User.js
+++ Models/User.js
@@ -1,23 +1,27 @@
-const mongoose=require("mongoose")
-const UserSchema=new mongoose.Schema({
- name:{
-    type:String,
-   required:true
- },
- email:{
-    type:String,
-    required:true,
-    unique:true
- },
- password:{
-    type:String,
-    required:true
- },
- date:{
-    type:Date,
-    default:Date.now
- }
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-})
-const User=new mongoose.model("User",UserSchema);
-module.exports=User;
+const User = new mongoose.model("User", UserSchema);
+
+
+User.collection.createIndex({ email: 1 }, { unique: true }); 
+
+module.exports = User;
